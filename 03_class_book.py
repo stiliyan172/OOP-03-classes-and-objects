fbe23075f84a105d4c621d9532892c93e8a80279
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-02-24 14:57:39 +0200

    Delete 03_class_book.py

--- 03_class_book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, name, author, pages):
-        self.name = name
-        self.author = author
-        self.pages = pages
-
-
-book_1 = Book("Steve Jobs", "me", 22)
-print(book_1.name)
